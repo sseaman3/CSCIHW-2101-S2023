29c85c305b1018ac913af7a67648bb6b7aa59c32
Sarah Seaman <seamans@go.stockton.edu>
2023-03-03 01:57:45 -0500

    Commit

--- HW1/Main.java
+++ HW1/Main.java
@@ -3,11 +3,16 @@ package HW1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start of Main");
-        // Create an instance of the Pyramid class and call the makePyramid method (see Pyramid.java for more info)
-
+        // Create an instance of the Pyramid class and call the Output method (see Pyramid.java for more info)
+Pyramid a = new Pyramid();
+a.Output();
         // Create an instance of the Adlibs class and call the story method (see Adlibs.java for more info)
-
+Adlibs b = new Adlibs();
+b.story("Sarah", 18, "pink");
         // Create an instance of the Replace class and call the charReplace method (see Replace.java for more info)
+Replace c = new Replace();
+c.charReplace("Hello", 4, 'a');
+        
 
     }
 }
